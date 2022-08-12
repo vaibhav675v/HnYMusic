2a2c756db6053c5227bfdcda655c5b10c86af7c3
Vaibhav <98152159+vaibhav675v@users.noreply.github.com>
2022-08-12 19:16:19 +0530

    Update ping.py

--- HnYMusic/Helpers/Inline/ping.py
+++ HnYMusic/Helpers/Inline/ping.py
@@ -11,7 +11,7 @@ ping_ig = InlineKeyboardMarkup(
                 ),
                 InlineKeyboardButton(
                     text="sᴏᴜʀᴄᴇ",
-                    url="https://github.com/HNYROBO/HnYMusic"
+                    url="https://telegra.ph/file/2614b9f734af35d9f7d2f.jpg"
                 )
             ]
         ]
